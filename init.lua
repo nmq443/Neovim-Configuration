88c27dd5b16294ca7192a97016eb1920013cbffa
nmq443 <21020564@vnu.edu.vn>
2024-03-15 22:14:14 +0700

    installed plugins

--- init.lua
+++ init.lua
@@ -1,5 +1,70 @@
+-- ========================================================================== --
+-- ==                               PLUGINS                                == --
+-- ========================================================================== --
+local lazy = {}
 
--- Appearance
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  if vim.g.plugins_ready then
+    return
+  end
+
+  -- You can "comment out" the line below after lazy.nvim is installed
+  -- lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+
+  require('lazy').setup(plugins, lazy.opts)
+  vim.g.plugins_ready = true
+end
+
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+
+lazy.setup({
+  {'folke/tokyonight.nvim'},
+  {'kyazdani42/nvim-web-devicons'},
+  {'nvim-lualine/lualine.nvim'},
+})
+
+-- ========================================================================== --
+-- ==                           EDITOR SETTINGS                            == --
+-- ========================================================================== --
 vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
 vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
 
+-- ========================================================================== --
+-- ==                         PLUGIN CONFIGURATION                         == --
+-- ========================================================================== --
+vim.cmd.colorscheme('tokyonight')
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+require('lualine').setup({
+  options = {
+    icons_enabled = false,
+    theme = 'tokyonight',
+    component_separators = '|',
+    section_separators = '',
+  },
+})
